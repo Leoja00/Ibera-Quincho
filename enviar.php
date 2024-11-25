4c88cf358ead27871671ae3054f73ad6ef90358f
Leonardo <leoja00@gmail.com>
2024-11-25 13:36:11 -0300

    mail

--- enviar.php
+++ enviar.php
@@ -1,39 +1,45 @@
 <?php 
-	$destinatario="leoja00@gmail.com";
-	$asunto="Contacto desde el sitio web";
+header('Content-Type: application/json'); 
 
+$destinatario = "leoja00@gmail.com";
+$asunto = "Contacto desde el sitio web";
 
-	$nombre=$_POST['nombre'];
-	$email=$_POST['email'];
-    $telefono=$_POST['telefono'];
-	$duda=$_POST['duda'];
+$nombre = htmlspecialchars($_POST['nombre'] ?? '');
+$email = htmlspecialchars($_POST['email'] ?? '');
+$telefono = htmlspecialchars($_POST['telefono'] ?? '');
+$duda = htmlspecialchars($_POST['duda'] ?? '');
 
+// Verifica que todos los campos tengan datos
+if (!empty($nombre) && !empty($email) && !empty($telefono) && !empty($duda)) {
+    $cuerpo = "
+        <html>
+            <head></head>
+            <body>
+                <h3>Solicitud de contacto desde el sitio web</h3>
+                <p>
+                    <strong>Nombre:</strong> $nombre<br>
+                    <strong>Correo:</strong> $email<br>
+                    <strong>Teléfono:</strong> $telefono<br>
+                    <strong>Consulta:</strong> $duda
+                </p>
+            </body>
+        </html>
+    ";
 
-	$cuerpo='
-		<html>
-			<head>
-			</head>
-			<body>
-				<h3>Solicitud de contacto con Ibera Quincho!</h3>
-				<p>
-					Nombre: '.$nombre.'<br>
-					Correo: '.$email.'<br>
-					Telefono: '.$telefono.'<br>
-					Consulta: '.$duda.'
-				</p>
-			</body>
-		</html>	
-	';
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+    $headers .= "From: $nombre <$email>\r\n";
 
-	
-
-	$headers="MIME-Version: 1.0\r\n";
-	$headers.="Content-type: text/html;charset=UTF8\r\n";
-
-	//dirrecion remitente
-	$headers.="FROM:$nombre <$email>\r\n";
-
-	mail($destinatario,$asunto,$cuerpo,$headers);
-	header('Location:index.html#contacto')
-
- ?> 
+    // Intenta enviar el correo
+    if (mail($destinatario, $asunto, $cuerpo, $headers)) {
+        echo json_encode(['success' => true]);
+        exit;
+    } else {
+        echo json_encode(['success' => false, 'message' => 'No se pudo enviar el correo.']);
+        exit;
+    }
+} else {
+    echo json_encode(['success' => false, 'message' => 'Faltan datos obligatorios.']);
+    exit;
+}
+?>
